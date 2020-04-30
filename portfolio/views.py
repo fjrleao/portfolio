738bcb8d0c20632faf566debde1d9fd0d615fcf5
Fabio Junior <fjrleao@gmail.com>
2020-04-30 09:57:01 -0300

    Adicao de formulario de contato

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Sessao, Texto, Card
+from .models import Sessao, Texto, Card, Modal
 
 def index(request):
     template = 'index.html'
     sessoes = Sessao.objects.all()
     textos = Texto.objects.all()
     cards = Card.objects.all()
+    modals = Modal.objects.all()
     context = {
         "sessoes" : sessoes,
         "textos" : textos,
-        "cards": cards
+        "cards": cards,
+        "modals": modals
     }
     return render(request, template, context)
