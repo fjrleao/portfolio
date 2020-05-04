1c9ce96012e521dfbd9c1642a51a09a9eb715b22
Fabio Junior <fjrleao@gmail.com>
2020-05-04 16:41:00 -0300

    Correcao de view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse
 from .models import Sessao, Texto, Card, Modal, Perfil, Link
 
 def index(request):
-    template = 'index.html'
-    sessoes = Sessao.objects.all()
-    textos = Texto.objects.all()
-    cards = Card.objects.all()
-    modals = Modal.objects.all()
-    perfil = Perfil.objects.first()
-    links = Link.objects.all()
-    context = {
-        "sessoes" : sessoes,
-        "textos" : textos,
-        "cards": cards,
-        "modals": modals,
-        "perfil": perfil,
-        "links" : links
-    }
-    return render(request, template, context)
+    try:
+        template = 'index.html'
+        sessoes = Sessao.objects.all()
+        textos = Texto.objects.all()
+        cards = Card.objects.all()
+        modals = Modal.objects.all()
+        perfil = Perfil.objects.first()
+        links = Link.objects.all()
+        context = {
+            "sessoes" : sessoes,
+            "textos" : textos,
+            "cards": cards,
+            "modals": modals,
+            "perfil": perfil,
+            "links" : links
+        }
+        return render(request, template, context)
+    except:
+        return HttpResponse('<h3>Erro inesperado, configure seus dados no banco para exibir a página</h3>')
 
