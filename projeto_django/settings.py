411384f33433b461a207153c7b051b4cd0dfd630
Fabio Junior <fjrleao@gmail.com>
2020-05-04 17:20:06 -0300

    Atualizaçao settings

--- projeto_django/settings.py
+++ projeto_django/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'projeto_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
