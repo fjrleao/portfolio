14db025b2a4228aa3a98a7b8df04adc757ced7fd
Fabio Junior <fjrleao@gmail.com>
2020-05-04 17:09:30 -0300

    Admin permitido

--- projeto_django/settings.py
+++ projeto_django/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.environ.get('DEBUG') == True
 
 ALLOWED_HOSTS = [
     'https://fjrleao.herokuapp.com/',
-    'https://fjrleao.herokuapp.com/*/'
+    'https://fjrleao.herokuapp.com/admin/portfolio/perfil/'
 ]
 
 
