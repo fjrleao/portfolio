141368ab1ed77a2cb1cec206d1c4e45f0cc77669
Fabio Junior <fjrleao@gmail.com>
2020-05-05 08:44:06 -0300

    Alteraçao em settings

--- projeto_django/settings.py
+++ projeto_django/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio'
+    'projeto_django.portfolio'
 ]
 
 MIDDLEWARE = [
